25f59c05988b8d609dfa6a06007362f47c9c7f34
Padma2Code <padmavalli.aluri@gmail.com>
2021-02-05 03:34:08 +0000

    parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+//query to return all teachers who assisted for a cohort
+const cohortName = process.argv[2];
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE assistance_requests.teacher_id = teachers.id 
+AND cohorts.name LIKE $1
+ORDER BY teacher;`
+pool.query(queryString, [`%${cohortName}%`])
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+  res.rows.forEach((data) => {
+    console.log(`${data.cohort}: ${data.teacher}`);
+  });
+})
+.catch(err => console.error('query error', err.stack));
